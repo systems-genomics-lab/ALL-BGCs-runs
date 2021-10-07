91c2ddfdd24189178e91ec3102599e767d2b64ff
Ali Elbehery <aelbehery@aucegypt.edu>
2021-10-08 01:00:19 +0200

    added condition to avoid annotation if no argonautes were detected

--- scripts/annotate_positive_contig.sh
+++ scripts/annotate_positive_contig.sh
@@ -6,8 +6,15 @@ sample=`echo $HMM_TBL | cut -d'.' -f1`
 
 awk '!/^#/ {print $1}' $HMM_TBL | rev | cut -d '_' -f2- | rev | sort | uniq > $sample.positive.contigs.txt
 
-mkdir $sample.positive.contigs.genes
-parallel -a $sample.positive.contigs.txt "awk  "/{}/" $sample.contigs_genes.faa | cut -d' ' -f1 | cut -d'>' -f2 > $sample.positive.contigs.genes/{}_genes.txt"
-ls $sample.positive.contigs.genes/*_genes.txt | parallel "seqtk subseq $sample.contigs_genes.faa {} > {.}.faa"
+if [ -s $sample.positive.contigs.txt ] ; then 
 
-ls $sample.positive.contigs.genes/*_genes.faa | parallel "svr_assign_using_figfams < {} &> {.}_annotations.txt"
+    mkdir $sample.positive.contigs.genes
+
+    parallel -a $sample.positive.contigs.txt "awk  "/{}/" $sample.contigs_genes.faa | cut -d' ' -f1 | cut -d'>' -f2 > $sample.positive.contigs.genes/{}_genes.txt"
+    ls $sample.positive.contigs.genes/*_genes.txt | parallel "seqtk subseq $sample.contigs_genes.faa {} > {.}.faa"
+
+    ls $sample.positive.contigs.genes/*_genes.faa | parallel "svr_assign_using_figfams < {} &> {.}_annotations.txt"
+else
+    echo "No contigs annotated because no argonaute were detected"
+
+fi
