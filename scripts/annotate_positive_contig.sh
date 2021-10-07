96d375b89b44c7708dc0ed006945ba35e3c93d25
Ali Elbehery <aelbehery@aucegypt.edu>
2021-10-08 00:07:02 +0200

    script to annotate the whole contigs from which one/more protein(s) was identified as being argonaute

---
+++ scripts/annotate_positive_contig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HMM_TBL=$1
+sample=`echo $HMM_TBL | cut -d'.' -f1`
+
+
+awk '!/^#/ {print $1}' $HMM_TBL | rev | cut -d '_' -f2- | rev | sort | uniq > $sample.positive.contigs.txt
+
+mkdir $sample.positive.contigs.genes
+parallel -a $sample.positive.contigs.txt "awk  "/{}/" $sample.contigs_genes.faa | cut -d' ' -f1 | cut -d'>' -f2 > $sample.positive.contigs.genes/{}_genes.txt"
+ls $sample.positive.contigs.genes/*_genes.txt | parallel "seqtk subseq $sample.contigs_genes.faa {} > {.}.faa"
+
+ls $sample.positive.contigs.genes/*_genes.faa | parallel "svr_assign_using_figfams < {} &> {.}_annotations.txt"
