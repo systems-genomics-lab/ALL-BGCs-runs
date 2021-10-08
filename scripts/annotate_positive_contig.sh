cd50b6cd48f40e60a19a8eee42cb3da12779b48b
Ali Elbehery <aelbehery@aucegypt.edu>
2021-10-08 14:26:25 +0200

    modified script to also extract argonaute proteins and report simple statistics about them

--- scripts/annotate_positive_contig.sh
+++ scripts/annotate_positive_contig.sh
@@ -4,9 +4,18 @@ HMM_TBL=$1
 sample=`echo $HMM_TBL | cut -d'.' -f1`
 
 
-awk '!/^#/ {print $1}' $HMM_TBL | rev | cut -d '_' -f2- | rev | sort | uniq > $sample.positive.contigs.txt
+#extract gene names and sequences
+awk '!/^#/ {print $1}' $HMM_TBL > $sample.positive.genes.txt
 
-if [ -s $sample.positive.contigs.txt ] ; then 
+if ! [ -s $sample.positive.genes.txt ] ; then
+    echo "No argonaute proteins detected"
+    exit 0
+else
+    seqtk subseq $sample.contigs_genes.faa $sample.positive.genes.txt > $sample.positive.genes.faa
+    awk -F '[ ;]' '/^>/ {if (seqlen){print seqlen}; printf substr($1,2) "\t" $10 "\t" ;seqlen=0; next; } { seqlen += length($0)}END{print seqlen}' $sample.positive.genes.faa | sed '1igene\tpartial\tlength' > $sample.positive.genes.stats.txt
+
+
+    awk '!/^#/ {print $1}' $HMM_TBL | rev | cut -d '_' -f2- | rev | sort | uniq > $sample.positive.contigs.txt
 
     mkdir $sample.positive.contigs.genes
 
@@ -14,7 +23,6 @@ if [ -s $sample.positive.contigs.txt ] ; then
     ls $sample.positive.contigs.genes/*_genes.txt | parallel "seqtk subseq $sample.contigs_genes.faa {} > {.}.faa"
 
     ls $sample.positive.contigs.genes/*_genes.faa | parallel "svr_assign_using_figfams < {} &> {.}_annotations.txt"
-else
-    echo "No contigs annotated because no argonaute were detected"
+    
 
 fi
